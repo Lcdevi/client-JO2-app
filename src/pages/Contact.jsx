cd2dc074432c4753bf73aa5819bbaaceaec55264
Lcdevi <caillauxlaura@gmail.com>
2020-11-20 20:42:28 +0100

    begin contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import "../styles/contact.css";
 
 class Contact extends Component {
 
   render() {
     return (
-      <h1>TEST</h1>
+      <div>
+        <h1>TEST</h1>
+        <form>
+          Nom
+          <input type="text" name="" id="" placeholder="nom"/>
+          Prénom
+          <input type="text" name="" id="" placeholder="prénom"/>
+          Email
+          <input type="text" name="" id="" placeholder="email"/>
+          Sujet
+          <input type="text" name="" id="" placeholder="sujet"/>
+          Message
+          <textarea name="" id="" cols="30" rows="10" placeholder="votre message ici"></textarea>
+          <button>Envoyer</button>
+        </form>
+      </div>
     )
   }
 }
